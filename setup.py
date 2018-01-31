4ba89ddcca8aac6c7dd2b10f89ddb9a7e8a27e03
Aly Sivji <4369343+alysivji@users.noreply.github.com>
2018-01-31 00:18:30 -0600

    Release package on PyPI (#5)

--- setup.py
+++ setup.py
@@ -1,17 +1,29 @@
 from setuptools import setup, find_packages
 
+from sivtools import __version__
+
 setup(
     name='sivtools',
-    version='0.0.1',
-    description="Sivji's Toolbox",
+    version=__version__,
+    description="General purpose toolbox",
+    long_description="Pramatic programmers have their own toollbox",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        # 'Programming Language :: Python :: 2.7', #TODO get it working for py2
+        'Topic :: Utilities',
+    ],
+    keywords='toolbox utilities',
     url='https://github.com/alysivji/sivtools',
+    download_url='https://pypi.python.org/pypi/sivtools/',
     author='Aly Sivji',
     author_email='alysivji@gmail.com',
-    classifiers=[
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 2.7',
-    ],
+    license='MIT',
     packages=find_packages(exclude=['tests', ]),
     install_requires=[''],
-    download_url='https://github.com/alysivji/sivtools',
 )
