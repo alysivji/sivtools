cd314a2c0ebcf45cc2f0d2f8485471c264499aca
Aly Sivji <alysivji@gmail.com>
2018-06-09 19:20:28 -0500

    Fix RTD formatting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,9 +6,6 @@
 # full list see the documentation:
 # http://www.sphinx-doc.org/en/master/config
 
-# -- Import ------------------------------------------------------------------
-import sphinx_theme
-
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -78,8 +75,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "neo_rtd_theme"
-html_theme_path = [sphinx_theme.get_html_theme_path(html_theme)]
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
